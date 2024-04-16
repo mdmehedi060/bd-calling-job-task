1d0df627fef78666061c7be9a75b29d7cd9bc081
mdmehedi060 <mdmehedi060@gmail.com>
2024-04-17 00:24:58 +0600

    color change

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <div className="navbar bg-[#5A1BFF] my-10">
+    <div className="navbar bg-[#7079FF] my-10">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -33,7 +33,7 @@ const Navbar = () => {
               <a>About Us</a>
             </li>
             <li>
-              <a>Contract</a>
+              <a>Contact</a>
             </li>
           </ul>
         </div>
